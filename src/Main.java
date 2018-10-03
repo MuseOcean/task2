70ab4f594849de2043d9f537ea910f7a8bbb372d
Alex Moonk <conte_alex@mail.ru>
2018-10-04 01:28:23 +0300

    create developer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
+
+    byte number1 = 1, number2 = 2;
+    int sum;
+    byte value1 = 0, value2 = 1;
+    boolean result;
+
+    byte num;
+    double variable = 56.6;
+
+    char m = 105;
+
+    String hi = "Hello, how low? ";
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        sum = number1 + number2;
+        System.out.println(sum);
+
+        result = value1 < value2;
+        System.out.println(result);
+
+        System.out.println(num++);
+
+        variable += 15.8;
+        System.out.println(variable);
+
+
+        System.out.println(hi + "makes");
+
+
+
+
     }
 }
